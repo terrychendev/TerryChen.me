a1dc72ae22e9b3774d5a3a69c8a8798c03699bb0
Terry Chen <terrychen@Terrys-MacBook-Pro.local>
2014-05-13 11:18:21 -0400

    add Favicon.ico

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ else if (environments == "production"){
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
